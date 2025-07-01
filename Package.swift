adc76a04872b880e5f890d597ea716d2b293ffe7
sheikh0687 <arbazsheikh99@gmail.com>
2025-07-01 17:43:42 +0530

    Organized source files into Sources/RangeSeekSlider

--- Package.swift
+++ Package.swift
@@ -1,5 +1,21 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "RangeSeekSlider",
+            path: "Sources"
+        ),
+    ]
 )
